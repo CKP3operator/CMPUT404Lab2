501ed1fb41181f91bf352a4a3c625b2beb61fc30
CKP3operator <perezcharles1112@gmail.com>
2023-09-20 19:24:58 -0600

    done lab 2

--- echo_server.py
+++ echo_server.py
@@ -1,33 +1,27 @@
-#!/usr/bin/env python3
 import socket
-import time
+from threading import Thread
 
-#define address & buffer size
-HOST = ""
-PORT = 8001
-BUFFER_SIZE = 1024
+BYTES_TO_READ = 4096
+HOST = "127.0.0.1" # IP # localhost
+PORT = 8080
 
-def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    
-        #QUESTION 3
+def handle_connection(conn,addr):
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(BYTES_TO_READ) # wait for a request and when you get it, you receive it
+            if not data: #if I receive an empty byte string ==> b
+                break
+            print(data)
+            conn.sendall(data) #send it back to client
+
+def start_threaded_server():
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+        s.bind((HOST,PORT))
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        
-        #bind socket to address
-        s.bind((HOST, PORT))
-        #set to listening mode
         s.listen(2)
-        
-        #continuously listen for connections
         while True:
             conn, addr = s.accept()
-            print("Connected by", addr)
-            
-            #recieve data, wait a bit, then send it back
-            full_data = conn.recv(BUFFER_SIZE)
-            time.sleep(0.5)
-            conn.sendall(full_data)
-            conn.close()
-
-if __name__ == "__main__":
-    main()
+            thread = Thread(target = handle_connection, args=(conn, addr))
+            thread.run()
+start_threaded_server()
